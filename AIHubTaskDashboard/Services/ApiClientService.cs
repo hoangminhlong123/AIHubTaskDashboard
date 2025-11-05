4938bad1a36710ff6dfaf1f079885d1e50f32dba
letrongphu18 <trphu1810@gmail.com>
2025-11-05 10:12:30 +0700

    checkout

--- AIHubTaskDashboard/Services/ApiClientService.cs
+++ AIHubTaskDashboard/Services/ApiClientService.cs
@@ -172,4 +172,4 @@ namespace AIHubTaskDashboard.Services
 			}
 		}
 	}
-}
+} 
--- AIHubTaskDashboard/obj/Debug/net8.0/AIHubTaskDashboard.AssemblyInfo.cs
+++ AIHubTaskDashboard/obj/Debug/net8.0/AIHubTaskDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHubTaskDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3f5ca35aa31f502653921745ef496346fb85ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df176a2c7c539e697aa271252d351569b5865151")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHubTaskDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHubTaskDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
