1ef5838813ed12378aecc0f43db1721cde20d328
letrongphu18 <trphu1810@gmail.com>
2025-11-06 08:40:24 +0700

    update token

--- AIHubTaskDashboard/obj/Debug/net8.0/AIHubTaskDashboard.AssemblyInfo.cs
+++ AIHubTaskDashboard/obj/Debug/net8.0/AIHubTaskDashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AIHubTaskDashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df176a2c7c539e697aa271252d351569b5865151")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4938bad1a36710ff6dfaf1f079885d1e50f32dba")]
 [assembly: System.Reflection.AssemblyProductAttribute("AIHubTaskDashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AIHubTaskDashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
